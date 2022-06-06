b544e263392d752a9e3211e5c537ac95ff6faa2f
Vivek Bisht <99522211+beastvivek@users.noreply.github.com>
2022-06-06 17:57:05 +0530

    Used class for form.js

--- form.js
+++ form.js
@@ -1,5 +1,37 @@
 const fs = require('fs');
 
+class Form {
+  constructor(fields, validators, questions) {
+    this.fields = fields;
+    this.validators = validators;
+    this.questions = questions;
+    this.index = 0;
+    this.details = {};
+  }
+
+  #outOfIndex() {
+    return this.index >= this.fields.length;
+  }
+
+  #endStdIn() {
+    process.stdin.emit('end');
+  }
+
+  takeInput(detail) {
+    if (this.validators[this.index](detail)) {
+      this.details[this.fields[this.index]] = detail;
+      this.index++;
+      if (this.#outOfIndex()) {
+        return this.#endStdIn();
+      }
+      console.log(this.questions[this.index]);
+      return;
+    }
+    console.log('Invalid input');
+    console.log(this.questions[this.index]);
+  }
+}
+
 const isValidName = (name) => {
   return name.length >= 5 && !/[0-9]/.test(name);
 };
@@ -12,29 +44,30 @@ const areValidHobbies = (hobbies) => {
   return hobbies !== '';
 };
 
+const isPhoneNumberValid = (phoneNumber) => {
+  return /^\d.*$/.test(phoneNumber) && phoneNumber.length === 10;
+};
+
 const main = () => {
-  const details = {};
-  const fields = ['name', 'DOB', 'Hobbies'];
-  const validators = [isValidName, isValidDob, areValidHobbies];
+  const fields = ['name', 'dob', 'hobbies', 'phoneNumber'];
+  const validators = [
+    isValidName, isValidDob, areValidHobbies, isPhoneNumberValid
+  ];
+  const questions = [
+    'Please enter your name',
+    'Please enter your DOB',
+    'Please enter your hobbies',
+    'Please enter your phone number'
+  ];
   process.stdin.setEncoding('utf8');
-  let counter = 0;
-  console.log('Please enter your', fields[counter]);
+  const form = new Form(fields, validators, questions);
+  console.log('Please enter your', fields[0]);
   process.stdin.on('data', (chunk) => {
-    const userDetails = chunk.split('\n')[0];
-    if (validators[counter](userDetails)) {
-      details[fields[counter]] = userDetails;
-      counter++;
-      if (counter >= fields.length) {
-        fs.writeFileSync('./details.json', JSON.stringify(details), 'utf8');
-        process.stdin.emit('end');
-      }
-      console.log('Please enter your', fields[counter]);
-    } else {
-      console.log('Invalid Input');
-      console.log('Please enter your', fields[counter]);
-    }
+    const detail = chunk.split('\n')[0];
+    form.takeInput(detail);
   });
   process.stdin.on('end', () => {
+    fs.writeFileSync('./details.json', JSON.stringify(form.details), 'utf8');
     console.log('Thank You!!');
     process.exit();
   });
