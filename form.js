afd9d388ec73a0da98ec777d03160d666d261e56
Vivek Bisht <99522211+beastvivek@users.noreply.github.com>
2022-06-07 09:30:51 +0530

    Moved form class to a new file

--- form.js
+++ form.js
@@ -1,5 +1,3 @@
-const fs = require('fs');
-
 class Form {
   #index;
   #details;
@@ -52,43 +50,4 @@ class Form {
   }
 }
 
-const isValidName = (name) => {
-  return name.length >= 5 && !/[0-9]/.test(name);
-};
-
-const isValidDob = (dob) => {
-  return /^\d{4,4}-\d{2,2}-\d{2,2}$/.test(dob);
-};
-
-const areValidHobbies = (hobbies) => {
-  return hobbies !== '';
-};
-
-const isPhNumValid = (phoneNumber) => {
-  return /^\d.*$/.test(phoneNumber) && phoneNumber.length === 10;
-};
-
-const main = () => {
-  process.stdin.setEncoding('utf8');
-  const form = new Form();
-  form.addField('name', 'Please enter your name', isValidName);
-  form.addField('dob', 'Please enter your dob', isValidDob);
-  form.addField('hobbies', 'Please enter your hobbies', areValidHobbies);
-  form.addField('phNum', 'Please enter your phone number', isPhNumValid);
-
-  console.log(form.currentQuestion());
-
-  process.stdin.on('data', (chunk) => {
-    const detail = chunk.split('\n')[0];
-    form.addInput(detail);
-  });
-
-  process.stdin.on('end', () => {
-    const details = form.getDetails();
-    fs.writeFileSync('./details.json', JSON.stringify(details), 'utf8');
-    console.log('Thank You!!');
-    process.exit();
-  });
-};
-
-main();
+exports.Form = Form;
